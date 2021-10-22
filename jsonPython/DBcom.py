6e42fc719b2f9273cfbe406ac9a28dbf852d2e13
ryangithub1 <ryan.aaixe@gmail.com>
2021-10-22 13:29:15 +0800

    update

--- jsonPython/DBcom.py
+++ jsonPython/DBcom.py
@@ -1,4 +1,14 @@
 import json
 import os
-class UserDB():
-    def create(self, tablename, data):
+import uuid
+import base64
+class userDB():
+    def create(tablename, colName, colType, rowid, data):
+        path = ('db/' + tablename + '/' + colName)
+        os.makedirs(path, exist_ok=True)
+        data = base64.b64encode(data)
+        
+        
+        
+
+
