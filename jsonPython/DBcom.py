30bf7ad154e139cf0da9286d86d83d309e9c0fcb
ryangithub1 <ryan.aaixe@gmail.com>
2021-10-28 21:23:46 +0800

    update

--- jsonPython/DBcom.py
+++ jsonPython/DBcom.py
@@ -1,24 +1,25 @@
 import json
-import os
-import base64
-import re
+import os #navigate file system
+import base64 #ensures that each character is supported by the file system
+import re #use to manipulate strings
 class UserDB():
-    def create(tablename, colName, colType, rowid, data):
-        path = ('db/' + tablename + '/' + colName)
+    def create(tableName, colName, colType, rowid, data):
+        path = ('./db/' + tableName + '/' + colName)
+        
         os.makedirs(path, exist_ok=True)
         data = (data.encode('utf-8'))
         data = base64.b64encode(data)
-        filename = str(colType) + '_' + str(rowid) + '_' + str(data)
-        with open(path +'/'+ filename + '_' + data) as f:
+        filename = str(rowid) + '_' + str(colType) + '_' + str(data)[2:-1]
+        with open(path +'/'+ filename, 'w+') as f:
             f.write(str(data))
         return rowid
 
-    def find(tablename, colName, data):
+    def find(tableName, colName, data):
         results = []
         data = (data.encode('utf-8'))
         data = str(base64.b64encode(data))[2:-3]
         regex = re.compile(data)
-        for root, dirs, files in os.walk('db/'+ tablename+'/'+ colName):
+        for root, dirs, files in os.walk('db/'+ tableName+'/'+ colName):
             for file in files:
                 file_data = file.split('_')[2]
                 if bool(re.match(regex, file_data)):
@@ -26,10 +27,10 @@ class UserDB():
                     results.append(file.split('_')[0])
         return results
 
-    def find_rowid(tablename, colName, rowid):
+    def find_rowid(tableName, colName, rowid):
         results = []
         regex = re.compile(rowid)
-        path = ('db/' + tablename + '/' + colName)
+        path = ('db/' + tableName + '/' + colName)
         for root, dirs, files in os.walk(path):
             for file in files:
                 file_data = file.split('_')[0]
@@ -38,8 +39,8 @@ class UserDB():
                     results.append(file.split('_')[0])
         return results
 
-    def read(tablename, colName, rowid):
-        path = ('db/' + tablename + '/' + colName)
+    def read(tableName, colName, rowid):
+        path = ('db/' + tableName + '/' + colName)
         for root, dirs, files in os.walk(path):
             for file in files:
                 if file.split('_')[0] == str(rowid):
@@ -47,8 +48,8 @@ class UserDB():
                     data = str(data)[2:-1]
                     return data     
 
-    def update(tablename, colName, colType, rowid, data, colType2, rowid2, data2, userinput):
-        path = ('db/' + tablename + '/' + colName)
+    def update(tableName, colName, colType, rowid, data, colType2, rowid2, data2, userinput):
+        path = ('db/' + tableName + '/' + colName)
         oldFileName = str(colType) + '_' + str(rowid) + '_' + str(data)
         newFileName = str(colType2) + '_' + str(rowid2) + '_' + str(data2)
         for root, dirs, files in os.walk(path):
@@ -61,8 +62,8 @@ class UserDB():
                     print('Error, could not update file...')
                     return False
 
-    def delete(tablename, colName, colType, rowid, data):
-        path = ('db/' + tablename + '/' + colName)
+    def delete(tableName, colName, colType, rowid, data):
+        path = ('db/' + tableName + '/' + colName)
         filename = str(colType) + '_' + str(rowid) + '_' + str(data)
         if os.path.exists(path + '/' + filename):
             os.remove(path + '/' + filename)
