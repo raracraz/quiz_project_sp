ee5c6875ce2a7cc96f3e1eeee7079fbc2a3a99d1
ryangithub1 <ryan.aaixe@gmail.com>
2021-11-01 20:22:11 +0800

    update

--- jsonPython/UserDatabase.py
+++ jsonPython/UserDatabase.py
@@ -2,19 +2,31 @@ import os
 import DBcom
 import uuid
 rowid = hash(uuid.uuid4())
-username = str(input('Please enter your username: '))
-def registerUser(rowid, username):
-    
+def registerUser(rowid):
+    acl = '0000'
+    try:
+        username = str(input('Please enter your username: '))
+    except ValueError:
+        print('Please enter a valid username')
+        registerUser(rowid, acl)
+    try:
+        password = str(input('Please enter your password: '))
+    except ValueError:
+        print('Please enter a valid password')
+        registerUser(rowid, acl)
+    DBcom.UserDB.create('users', 'acl', 's', rowid, acl)
     DBcom.UserDB.create('users', 'username', 's', rowid, username)
+    DBcom.UserDB.create('users', 'password', 's', rowid, password)
 
     #password = str(input('Please enter your password: '))
     
-def switch_demo(argument):
+def switch_demo(menu):
+    menu = input('Please choose a Option')
     switcher = {
         1: "Login",
         2: "Register",
         3: "Forget password",
     }
-    print (switcher.get(argument, "Invalid Option"))
+    print (switcher.get(menu, "Invalid Option"))
 
-
+registerUser(rowid)
