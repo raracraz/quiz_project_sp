2b007bc28cd194601a79f257253491b48da27df9
ryangithub1 <ryan.aaixe@gmail.com>
2021-11-02 09:09:56 +0800

    Update UserDatabase.py

--- jsonPython/UserDatabase.py
+++ jsonPython/UserDatabase.py
@@ -3,7 +3,7 @@ import DBcom
 import uuid
 rowid = hash(uuid.uuid4())
 def registerUser(rowid):
-    acl = '0000'
+    acl = 0000
     try:
         username = str(input('Please enter your username: '))
     except ValueError:
@@ -14,19 +14,37 @@ def registerUser(rowid):
     except ValueError:
         print('Please enter a valid password')
         registerUser(rowid, acl)
-    DBcom.UserDB.create('users', 'acl', 's', rowid, acl)
+    DBcom.UserDB.create('users', 'acl', 'i', rowid, acl)
     DBcom.UserDB.create('users', 'username', 's', rowid, username)
     DBcom.UserDB.create('users', 'password', 's', rowid, password)
 
     #password = str(input('Please enter your password: '))
     
-    #def switch_demo():
-    #    menu = int(input('Please choose a Option: '))
-    #    switcher = {
-    #        1: "Login",
-    #        2: "Register",
-    #        3: "Forget password",
-    #    }
-    #    print (switcher.get(menu, "Invalid Option" ))
+
+
+#let the user choose to login, register or forget password
+
+def switchUser(rowid, login, registerUser, forgetPassword):
+    print('Welcome to the The quiz')
+    print('1. Login')
+    print('2. Register')
+    print('3. Forget password')
+    print('4. Exit')
+    try:
+        choice = int(input('Please enter your choice: '))
+    except ValueError:
+        print('Please enter a valid choice')
+        switchUser(rowid)
+    if choice == 1:
+        login(rowid)
+    elif choice == 2:
+        registerUser(rowid)
+    elif choice == 3:
+        forgetPassword(rowid)
+    elif choice == 4:
+        exit()
+    else:
+        print('Please enter a valid choice')
+        switchUser(rowid)
 
     
